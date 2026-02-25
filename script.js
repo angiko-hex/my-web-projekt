a3ef2585f742c6d02435e9dd1b9374409169047d
Angiko <koszegi.angela@gmail.com>
2026-02-25 19:20:33 +0100

    Javascript ujra javítva

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("#heme-toggle");
 btn.addEventListener("click", ()=>{
-    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('dark-mode');
     console.log("mód átváltva");//segít a hibakeresésben
 });
